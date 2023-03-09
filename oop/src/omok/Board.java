99f06702979ea89b67f2d1799b046a5b58f956e9
Yongseok <yongseok.dev@gmail.com>
2023-03-09 17:25:47 +0900

    oop-omok step1 좌표적용

--- oop/src/omok/Board.java
+++ oop/src/omok/Board.java
@@ -13,10 +13,17 @@ public class Board {
     }
     public void print() {
         for (int row = 0; row < size; row++) {
+        	System.out.print(row<10?" "+row+"|":row+"|");
             for (int col = 0; col < size; col++) {
                 System.out.print(" " + map[row][col]);
             }
             System.out.println();
         }
+
+        System.out.print("    ");
+        for(int i=0;i<size;i++) {
+        	System.out.print((char)('A'+i)+" ");            	
+        }
+        System.out.println();
     }
 }
