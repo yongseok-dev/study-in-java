a22ccf5b7eb21693bb82e23dae63b32f2848a116
Yongseok <yongseok.dev@gmail.com>
2023-03-09 17:24:29 +0900

    oop-omok 초기조건 세팅

--- oop/src/omok/Omok.java
+++ oop/src/omok/Omok.java
@@ -2,12 +2,12 @@ package omok;
 
 public class Omok {
     public static void main(String[] args) {
-        Player user = new Player("사용자", "O");
-        Player computer = new Player("컴퓨터", "X");
+        Player userA = new Player("사용자A", "●");
+        Player userB = new Player("사용자B", "○");
         Board board = new Board(19);
-        play(board, user, computer);
+        play(board, userA, userB);
     }
-    private static void play(Board board, Player user, Player computer) {
+    private static void play(Board board, Player userA, Player userB) {
         board.print();
     }
 }
